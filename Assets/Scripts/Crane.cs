a2da9bcf6e63ab62610143b27938b7befce905a6
Senitt <piotr.k.tor@gmail.com>
2018-12-01 01:34:43 +0100

    crane happy killer

--- Assets/Scripts/Crane.cs
+++ Assets/Scripts/Crane.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class Crane : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float rangeMove = 5f;
+    public float speedCrane = 1f;
+    public GameObject box;
+    public bool testDrop = false;
+    public Vector3 respawnPosition;
+    public GameObject staticBox;
+
+    bool moveXPlus = false;
+    float startPositionX;
+    
+
+	void Start ()
+    {
+        startPositionX = transform.position.x;	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (transform.position.x >= startPositionX + rangeMove)
+        {
+            moveXPlus = false;
+            staticBox.SetActive(true);
+        }
+        else if (transform.position.x <= startPositionX - rangeMove)
+        {
+            moveXPlus = true;
+            staticBox.SetActive(true);
+        }
+
+        if (moveXPlus)
+        {
+            transform.Translate(speedCrane, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-speedCrane, 0, 0);
+        }
+        if (testDrop)
+        {
+            InstantiateBox();
+            testDrop = false;
+            staticBox.SetActive(false);
+        }
+    }
+
+    void InstantiateBox()
+    {
+        
+        Instantiate(box, transform.position+respawnPosition, transform.rotation);
+    }
+
 }
