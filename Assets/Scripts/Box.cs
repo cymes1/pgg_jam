da2875d8e9cc4d384bb6ecd5fd181eb6058f5196
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-12-01 05:48:19 +0100

    Added Box script

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction //temp
+{
+	Left, Right
+}
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Box : MonoBehaviour 
+{
+	[SerializeField]
+	private BoxDirectionTrigger Left;
+	[SerializeField]
+	private BoxDirectionTrigger Right;
+	[SerializeField]
+	private BoxDirectionTrigger Up;
+
+	public bool CanBeMoved(Direction direction)
+	{
+		if(Up.IsTriggered)
+		{
+			return false;
+		}
+
+		if(direction == Direction.Left)
+		{
+			return !Left.IsTriggered;
+		}
+		else
+		{
+			return !Right.IsTriggered;
+		}
+	}
+
+	public bool CanClimb()
+	{
+		return !Up.IsTriggered;
+	}
+
+	public Box GetObject()
+	{
+		return this;
+	}
+}
