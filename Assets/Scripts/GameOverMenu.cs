6a65fdf94bae4a6360e76aea672ed46be0d41948
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-12-01 15:50:09 +0100

    Added player to scene

---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour 
+{
+	public void ReturnToMainMenu()
+	{
+
+	}
+
+	public void Retry()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
