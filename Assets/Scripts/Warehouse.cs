930dc77bd92e8ca0e4d565e3a2eb0b26f36e4b4e
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-12-01 05:50:27 +0100

    Change size of level in script

--- Assets/Scripts/Warehouse.cs
+++ Assets/Scripts/Warehouse.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class Warehouse : MonoBehaviour 
 {
-	public const int width = 16;
-	public const int height = 7;
+	public const int width = 10;
+	public const int height = 3;
 }
